2621b9c528819b6896e67d7b46550214cb8204b4
shiya0717 <xt16839688@gmail.com>
2026-03-12 12:01:56 +0800

    add Q123

--- week03/ex_total.java
+++ week03/ex_total.java
@@ -4,7 +4,7 @@ public class ex_total {
         int total = 0;
         int a=1 , b=10;
         total = sumN1N(a, b);
-        System.out.print(a + " ~ " + b + " total : " + total);
+        System.out.print(a + "~" + b + " total=" + total);
     }
     static int sumN1N(int begin, int end) {
         int i=0;
