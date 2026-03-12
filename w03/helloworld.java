4a8fac5ad34c48a64ae1d010b72baf311e22339b
shiya0717 <xt16839688@gmail.com>
2026-03-12 10:16:51 +0800

    add w02

---
+++ w03/helloworld.java
@@ -0,0 +1,14 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("\nHello,World!\n");
+    }
+    public void myfun(String[] args) {
+        System.out.println("\nmyfun is called\n");
+    }
+}
+
+class myclass {
+    public void myclass_myfun(String[] args) {
+        System.out.println("\nmyclass_myfun is called\n");
+    } 
+}
