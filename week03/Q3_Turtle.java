c2c75abebff0481c825ae57c5a271173112e9abf
shiya0717 <xt16839688@gmail.com>
2026-03-26 12:04:43 +0800

    123

--- week03/Q3_Turtle.java
+++ week03/Q3_Turtle.java
@@ -6,9 +6,11 @@ public class Q3_Turtle {
         this.species = species;
         this.age = age;
     }
+    
     public void showDetails() {
         System.out.println("\n品種：" + species + "，年紀：" + age + "歲");
     }
+
     public static void main(String[] args) {
         Q3_Turtle turtle = new Q3_Turtle("綠蠵龜", 50);
         turtle.showDetails();
